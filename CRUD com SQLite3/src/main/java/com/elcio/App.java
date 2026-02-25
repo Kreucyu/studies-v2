90cc653b956537a1177fb47b399a96e53c955140
Kreucyu <elciocleiton72@gmail.com>
2026-02-25 13:47:47 -0300

    Database created

--- CRUD com SQLite3/src/main/java/com/elcio/App.java
+++ CRUD com SQLite3/src/main/java/com/elcio/App.java
@@ -1,13 +1,14 @@
 package com.elcio;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.elcio.repository.DatabaseManager;
+import com.elcio.service.JogoService;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("CRUD simples:\n");
+        DatabaseManager manager = new DatabaseManager();
+        manager.inicializarBanco();
+        JogoService service = new JogoService();
+        service.function();
     }
 }
